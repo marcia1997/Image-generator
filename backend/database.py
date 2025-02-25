b1ef853957e0abb2d043af1eadbee901522fd8c2
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-02-25 23:25:13 +0200

    database update

--- backend/database.py
+++ backend/database.py
@@ -11,6 +11,8 @@ engine = create_async_engine(DATABASE_URL, echo=True)
 # Session factory
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
+async_session = SessionLocal
+
 # Base model for ORM
 Base = declarative_base()
 
